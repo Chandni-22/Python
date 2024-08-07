d02611206c01cf82550136a642ca3db0e702edb4
Chandni-22 <chandni22@navgurukul>
2024-08-07 10:32:36 +0530

    text colour change explanation

--- extra.py
+++ extra.py
@@ -9,6 +9,8 @@ cyan:36
 white:37'''
 
 # print("Uh, oh, you've been given a", "\033[31m", "warning", "\033[0m", "for being a bad, bad person.")
+# print("\033[30m","warning", "\033[31m", "\nwarning","\033[32m", "\nwarning","\033[33m", "\nwarning","\033[34m", "\nwarning","\033[35m", "\nwarning","\033[36m", "\nwarning","\033[37m", "\nwarning")
+
 
 '''
 "\033[31m": This is an escape sequence used to change the color of the text in the terminal. \033 is the escape character, [ starts the color code, 31 is the code for red, and m ends the code. When printed, it tells the terminal to switch the text color to red.
